4eef6de67c74f1d3c51e0c22de6a79ae720c0660
Nazmul Hossain <hossainnazmul191@gmail.com>
2023-10-06 22:51:53 +0600

    inventory management system project create for assignment

---
+++ src/views/ProductView.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import Layout from "../components/Layout.vue"
+    import ProductList from "../components/Product/ProductList.vue";
+</script>
+<template>
+        <Layout>
+            <ProductList></ProductList>
+        </Layout>
+</template>
+<style scoped>
+</style>
